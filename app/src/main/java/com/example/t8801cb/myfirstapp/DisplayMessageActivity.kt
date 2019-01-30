45c15fe96142ef4dac62c9cda3941cc4da83e4b2
Bebey-Tongo Cyrille Martin (FCA) <t8801cb@cag.cdroot.net>
2019-01-29 21:01:15 -0500

    added display message activity

--- app/src/main/java/com/example/t8801cb/myfirstapp/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/t8801cb/myfirstapp/DisplayMessageActivity.kt
@@ -1,12 +1,22 @@
 package com.example.t8801cb.myfirstapp
-
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.t8801cb.myfirstapp.R.id.text
 
 class DisplayMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
+
+        // Get the Intent that started this activity and extract the string
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // Capture the layout's TextView and set the string as its text
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
     }
 }
